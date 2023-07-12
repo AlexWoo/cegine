d6b3f050cc837094648c4b3b78726cedcd19fb99
AlexWoo <wj19840501@gmail.com>
2023-07-12 08:06:34 +0800

    ad NE CSTREQ CSTRNE Macro

--- tester/ce_tester.h
+++ tester/ce_tester.h
@@ -2,6 +2,7 @@
 #define __CE_TESTER_H__
 
 #include <sys/time.h>
+#include <string.h>
 
 #include <list>
 #include <string>
@@ -18,6 +19,17 @@ namespace CETester {
 
 #define EXPECT_EQ(v1, v2)                                               \
     AddRecord(new TesterRecord(std::string(#v1)+"=="+#v2, __LINE__, v1==v2))
+
+#define EXPECT_NE(v1, v2)                                               \
+    AddRecord(new TesterRecord(std::string(#v1)+"!="+#v2, __LINE__, v1!=v2))
+
+#define EXPECT_CSTR_EQ(s1, s2)                                          \
+    AddRecord(new TesterRecord(std::string(#s1)+"=="+#s2, __LINE__,     \
+    strlen(s1)==strlen(s2) && memcmp(s1, s2, strlen(s1)) == 0))
+
+#define EXPECT_CSTR_NE(s1, s2)                                          \
+    AddRecord(new TesterRecord(std::string(#s1)+"!="+#s2, __LINE__,     \
+    strlen(s1)!=strlen(s2) || memcmp(s1, s2, strlen(s1)) != 0))
 
 }
 
--- tester/test/main.cpp
+++ tester/test/main.cpp
@@ -2,21 +2,26 @@
 
 using namespace CETester;
 
-CE_TESTCASE(Test) {
-    std::cout << "Hello World" << std::endl;
+CE_TESTCASE(TestEQNE) {
+    int i = 10, j = 20;
 
     EXPECT_EQ(1, 1);
     EXPECT_EQ(20, 20.0);
+    EXPECT_EQ("Hello", "Hello");
+
+    EXPECT_NE("Hello", "World");
+    EXPECT_NE(i, j);
 }
 
-CE_TESTCASE(Test2) {
-    int i = 10, j = 20;
+CE_TESTCASE(TestSTREQNE) {
+    char *s1  = (char *)"Hello";
+    char s2[] = "Hello";
+    char *s3  = (char*)"World";
 
-    std::cout << "Hello World 2" << std::endl;
-
-    EXPECT_EQ("Hello", "World");
-    EXPECT_EQ("Hello", "Hello");
-    EXPECT_EQ(i, j);
+    EXPECT_NE(s1, s2);
+    EXPECT_CSTR_EQ(s1, s2);
+    EXPECT_CSTR_NE(s1, s3);
+    EXPECT_CSTR_NE(s2, s3);
 }
 
 int main() {
